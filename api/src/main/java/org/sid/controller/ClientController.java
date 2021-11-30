61c7e927ad0643c2caa319d4bfe442d8ec657278
hamadou Mballo <mballos1005@gmail.com>
2021-11-30 22:07:12 +0000

    implémentation des méthodes CRUD sur clientController.

--- api/src/main/java/org/sid/controller/ClientController.java
+++ api/src/main/java/org/sid/controller/ClientController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping(value = "/m2vehicule/api/client/*")
+//@RequestMapping(value = "/m2vehicule/api/client/*")
 public class ClientController {
 
     @Autowired
